5eddc695b20ae37d7d006f3951b6b24839830aff
Dav999 <dav999@tolp2.nl>
2019-03-10 00:50:17 +0100

    Command line arg for level can now be full path to level in levels dir
    Being able to open level files anywhere in your filesystem may be added
    later, but from now on it's possible to specify the full path of a
    level file as a CLI argument, as long as it's inside your levels
    folder.

--- Source/main2.lua
+++ Source/main2.lua
@@ -1372,6 +1372,9 @@ function love.update(dt)
 
 	if state == -2 then
 		if opt_loadlevel ~= nil then
+			if opt_loadlevel:sub(1, levelsfolder:len()) == levelsfolder then
+				opt_loadlevel = opt_loadlevel:sub(levelsfolder:len()+2, -1)
+			end
 			state6load(opt_loadlevel:sub(1,-8))
 			opt_loadlevel = nil -- If the level was invalid, we will still be in this state, and be redirected to state 6
 		elseif opt_newlevel then
