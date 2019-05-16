9c1d237b83a4ef626fabe30d34a3d245f9dac519
Info Teddy <infoteddyatgitgud@cock.li>
2019-05-16 15:46:57 -0700

    Fix character underneath cursor flashing white
    Now, when the cursor is in the part of its flash cycle where it's not
    rendering, it will not make the character underneath flash white, too.

--- Source/scriptfunc.lua
+++ Source/scriptfunc.lua
@@ -114,7 +114,9 @@ function syntaxhl(text, x, y, thisistext, addcursor, docolor, lasttextcolor, tex
 		if addcursor then
 			setColorArr(s.syntaxcolor_cursor)
 			if docolor then
-				love.graphics.print(firstUTF8(__), x+((string.len(text)-string.len(text_r))*(textsize and 16 or 8)), y)
+				if cursorflashtime <= .5 then
+					love.graphics.print(firstUTF8(__), x+((string.len(text)-string.len(text_r))*(textsize and 16 or 8)), y)
+				end
 			else
 				love.graphics.print(__, x+((offsetchars-1-string.len(text_r))*(textsize and 16 or 8)), y)
 			end
