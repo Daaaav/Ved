c6c1001911e9e0acdc8931a80eb16ea05b97a0a1
Info Teddy <infoteddyatgitgud@cock.li>
2019-06-20 18:08:37 -0700

    Use love.window.getDesktopDimensions()
    It turns out LO"VE *does* have this function. It just failed to come up
    in my search queries since I didn't get close enough with my words.

--- Source/main2.lua
+++ Source/main2.lua
@@ -81,15 +81,7 @@ function love.load()
 	if s.pscale ~= 1 or s.psmallerscreen then
 		za,zb,zc = love.window.getMode()
 		zd,ze,zf = love.window.getPosition()
-		-- Fun, ugly hack to determine the display height and width
-		-- based off of position alone, because LO"VE doesn't seem to
-		-- have functions to automatically do that
-		zwidth = 2*zd+za
-		if s.psmallerscreen then
-			zwidth = zwidth + 96
-		end
-		zheight = 2*ze+zb
-		cons("DISPLAY DIMENSIONS DETECTED: " .. zwidth .. "x" .. zheight)
+		zwidth,zheight = love.window.getDesktopDimensions(zf)
 		love.window.setMode(za*s.pscale,zb*s.pscale,zc)
 		love.window.setPosition((zwidth-za*s.pscale)/2,(zheight-zb*s.pscale)/2,zf)
 
