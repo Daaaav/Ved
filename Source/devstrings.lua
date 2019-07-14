6e10550a8ec931a61f2f69dea50cdece974a69e9
Info Teddy <infoteddyatgitgud@cock.li>
2019-07-14 04:19:00 -0700

    Add more descriptive behaviors to L.CONFIRMBIGGERSIZE
    In particular, you do not get more rooms or room properties from making
    the map bigger than 20x20, and rooms and room properties simply wrap
    around instead.
    
    This gets very close to not fitting in the dialog space (any more lines
    would simply not show up on the screen at all, instead of trailing off
    of the dialog), especially since I added two newlines to separate
    "VVVVVV can also crash for any reason in those rooms" from the preceding
    paragraph. My justification is that it's a bit hard to compress all the
    information into fewer words, plus if I write a lot of words it'll look
    scary and spook some people who don't know what they're doing from
    actually making bigger than 20x20 maps (although, really, I wanna more
    see bigger than 20x20 maps than not).
    
    Hope the translators have fun fitting the text into space. Or maybe
    Ved's dialogs will just resize to fit bigger content, who knows.

--- Source/devstrings.lua
+++ Source/devstrings.lua
@@ -2,7 +2,7 @@
 -- This file gets loaded after the language files do, so keys can overwrite existing ones, and will show up in all languages.
 
 --L. = ""
-L.CONFIRMBIGGERSIZE = "You are selecting $1 by $2, which is a bigger map size than 20 by 20. This will result in different behavior in rooms outside the normal 20x20 size, which Ved will mirror. VVVVVV can also crash for any reason in those rooms.\n\nPress Yes if you know what you're doing and want this bigger map size. Press No to set the map size to $3 by $4.\n\nIf unsure, press No."
+L.CONFIRMBIGGERSIZE = "You are selecting $1 by $2, which is a bigger map size than 20 by 20. Outside the normal 20 by 20 map, rooms and room properties wrap around, but are shifted up by one row of tiles and one row of rooms respectively, every 20 rooms. The new rows of tiles is some extra space, but you do not get entirely new rooms, nor do you get more room properties.\n\nVVVVVV can also crash for any reason in those rooms.\n\nPress Yes if you know what you're doing and want this bigger map size. Press No to set the map size to $3 by $4.\n\nIf unsure, press No."
 L.MAPBIGGERTHAN20X20 = "Map size $1 by $2 is bigger than 20 by 20! (Bigger than 20 by 20 support not enabled)"
 L.BTN_OVERRIDE = "Override"
 L.BTN_DONTOVERRIDE = "Don't Override"
