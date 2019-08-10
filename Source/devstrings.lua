a0b0814e46a94ef4b3e81cd2516df7ae3571b607
Dav999 <dav999@tolp2.nl>
2019-08-11 01:27:25 +0200

    Removed merge request devstrings, already in language files
    [pre16]

--- Source/devstrings.lua
+++ Source/devstrings.lua
@@ -2,9 +2,3 @@
 -- This file gets loaded after the language files do, so keys can overwrite existing ones, and will show up in all languages.
 
 --L. = ""
-L.OPAQUEROOMNAMEBACKGROUND = "Make the black roomname backgrounds opaque"
-L.PLATVCHANGE_TITLE = "Change platform speed"
-L.PLATVCHANGE_MSG = "Speed:"
-L.PLATVCHANGE_INVALID = "You have to type in a number."
-L.RENAMESCRIPTREFERENCES = "Rename references"
-L.PLATFORMSPEEDSLIDER = "Spd.:"
