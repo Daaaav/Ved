adcbe8aa0feb74641350113fa9efd9974dc1ce51
Dav999 <dav999@tolp2.nl>
2019-09-29 15:36:05 +0200

    Re-enable screensaver on LÖVE 0.10+, bumped Ved version to 1.7.0
    On LÖVE 0.10+, we can use love.window.setDisplaySleepEnabled(true) to
    re-enable the screensaver like SDL_EnableScreenSaver() should do.
    As I said in https://gitgud.io/Dav999/ved/commit/5d5d2c9e#note_76087,
    LÖVE 0.9.x will have the previous behavior of the screensaver being
    disabled by LÖVE.
    
    Also, I bumped the upcoming version number to 1.7.0. Lots of new
    features have been added already. This means we go from 1.6.2-pre14 to
    1.7.0-pre15.
    
    [pre15]

--- Source/enablescreensaver.lua
+++
@@ -1,7 +0,0 @@
-local ffi = require("ffi")
-
-ffi.cdef([[
-void SDL_EnableScreenSaver();
-]])
-
-ffi.C.SDL_EnableScreenSaver()
--- Source/main.lua
+++ Source/main.lua
@@ -1,6 +1,9 @@
---require("enablescreensaver")
 require("corefunc")
 require("coretext")
+
+if love_version_meets(10) then
+	love.window.setDisplaySleepEnabled(true)
+end
 
 allowdebug = false
 opt_disableversioncheck = false
@@ -8,7 +11,7 @@ opt_loadlevel = nil
 opt_newlevel = false
 opt_forcelanguagescreen = false
 
-vergroups = {6,2}
+vergroups = {7,0}
 
 ver = "1." .. vergroups[1] .. "." .. vergroups[2] -- Displayed in title and used in plugin minimum version check (egrep [^aotepk]ver[^socmdygwt] *.lua -i)
 checkver = ver -- update check, displayed in crash (used to have a or b as opposed to ver)
