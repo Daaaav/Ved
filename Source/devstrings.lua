5daeb3ae5669922396e8cd7288bd961d29ea4c67
Dav999 <dav999@tolp2.nl>
2020-01-30 22:09:20 +0100

    Shortened warning message when overriding >20x20 level size
    It was huge in English, other languages were definitely going to
    overflow the dialog. So I made it less detailed.
    
    [pre12]

--- Source/devstrings.lua
+++ Source/devstrings.lua
@@ -2,7 +2,7 @@
 -- This file gets loaded after the language files do, so keys can overwrite existing ones, and will show up in all languages.
 
 --L. = ""
-L.CONFIRMBIGGERSIZE = "You are selecting $1 by $2, which is a bigger map size than 20 by 20. Outside the normal 20 by 20 map, rooms and room properties wrap around, but are shifted up by one row of tiles and one row of rooms respectively, every 20 rooms. The new rows of tiles is some extra space, but you do not get entirely new rooms, nor do you get more room properties.\n\nVVVVVV can also crash for any reason in those rooms.\n\nPress Yes if you know what you're doing and want this bigger map size. Press No to set the map size to $3 by $4.\n\nIf unsure, press No."
+L.CONFIRMBIGGERSIZE = "You are selecting $1 by $2, which is a bigger map size than 20 by 20. Outside the normal 20 by 20 map, rooms and room properties wrap around, but are distorted. You do not get entirely new rooms, nor do you get more room properties. VVVVVV can also crash for any reason in those rooms.\n\nPress Yes if you know what you're doing and want this bigger map size. Press No to set the map size to $3 by $4.\n\nIf unsure, press No."
 L.MAPBIGGERTHAN20X20 = "Map size $1 by $2 is bigger than 20 by 20! (Bigger than 20 by 20 support not enabled)"
 L.BTN_OVERRIDE = "Override"
 L.BTN_DONTOVERRIDE = "Don't Override"
