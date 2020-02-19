ab8023852db00b66075c42e1338b1f73fed59910
Dav999 <dav999@tolp2.nl>
2020-02-19 01:34:55 +0100

    Adapted script editor for higher amounts of lines than VVVVVV supports
    The line counter can now count 4 digits instead of 3. And we don't want
    scripts to start on line 0001, especially not in VVVVVV levels, so the
    leading zeroes have been removed. Also, the line number doesn't become
    red anymore after 500 lines if the level target is VCE.
    
    [pre37]

--- Source/main2.lua
+++ Source/main2.lua
@@ -3783,13 +3783,13 @@ function love.mousepressed(x, y, button)
 				selectedsubtool[selectedtool] = 1
 			end
 		end
-	elseif state == 3 and button == "l" and nodialog and mouseon(48, 24, love.graphics.getWidth()-192, love.graphics.getHeight()-24) then
+	elseif state == 3 and button == "l" and nodialog and mouseon(56, 24, love.graphics.getWidth()-200, love.graphics.getHeight()-24) then
 		local chr, line
 		if s.scripteditor_largefont then
-			chr = math.floor((x-88)/16) + 1
+			chr = math.floor((x-104)/16) + 1
 			line = math.floor(((y-24)-scriptscroll-4)/16) + 1
 		else
-			chr = math.floor((x-48)/8) + 1
+			chr = math.floor((x-56)/8) + 1
 			line = math.floor(((y-24)-scriptscroll-6)/8) + 1
 		end
 		if chr < 1 then
