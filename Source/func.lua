Daaaav/Ved

commit a593569ec62490996245d5072d9dbb55e17d1a87

Allow using Home/End in file select dialogs
They will only work if you meet the (relaxed) scrollable requirements
changed in 9170f39ea4822a334544816e9c603a9942c8dbe5 AND if you are not
focused on a text field (otherwise, you'll just go to the left end/right
end of the text input).

Source/func.lua
@@ -2197,18 +2197,29 @@ function handle_scrolling(viakeyboard, mkinput, customdistance, x, y, gotothisen
 		if dialog.is_open() then
 			local topdialog = dialogs[#dialogs]
 			local k = topdialog:get_on_scrollable_field(x, y, viakeyboard)
+			local cf = dialogs[#dialogs].currentfield
+			local cfistext = anythingbutnil(dialogs[#dialogs].fields[cf])[6] == DF.TEXT
 			if k ~= nil then
 				local fieldscroll = topdialog.fields[k][10]
 				if direction == "u" then
-					fieldscroll = fieldscroll + distance
-					if fieldscroll > 0 then
+					if mkinput == "home" and not cfistext then
 						fieldscroll = 0
+					elseif mkinput == "pageup" then
+						fieldscroll = fieldscroll + distance
+						if fieldscroll > 0 then
+							fieldscroll = 0
+						end
 					end
 				elseif direction == "d" then
-					fieldscroll = fieldscroll - distance
-					local upperbound = (#topdialog.fields[k][7])*8-8*topdialog.fields[k][12]
-					if -fieldscroll > upperbound then
+					if mkinput == "end" and not cfistext then
+						local upperbound = (#topdialog.fields[k][7])*8-8*topdialog.fields[k][12]
 						fieldscroll = math.min(-upperbound, 0)
+					elseif mkinput == "pagedown" then
+						fieldscroll = fieldscroll - distance
+						local upperbound = (#topdialog.fields[k][7])*8-8*topdialog.fields[k][12]
+						if -fieldscroll > upperbound then
+							fieldscroll = math.min(-upperbound, 0)
+						end
 					end
 				end
 				dialogs[#dialogs].fields[k][10] = fieldscroll

Source/dialog.lua
@@ -309,6 +309,8 @@ function cDialog:keypressed(key)
 		self:press_button(DB.SAVE)
 	elseif key == "d" and self.buttons_present[DB.DISCARD] then
 		self:press_button(DB.DISCARD)
+	elseif table.contains({"home", "end"}, key) then
+		handle_scrolling(true, key, nil, nil, nil, key == "home" and "u" or "d")
 	end
 end
 

commit 46cc26b671a34a8066095763ea6ba983de033438

Restored ability to scroll in file pickers
According to git bisect it was caused by
a593569ec62490996245d5072d9dbb55e17d1a87. Only pageup and pagedown were
accounted for there, for some reason.

[pre25]

Source/func.lua
@@ -2130,7 +2130,7 @@ function handle_scrolling(viakeyboard, mkinput, customdistance, x, y)
 			if direction == "u" then
 				if mkinput == "home" and not cfistext then
 					fieldscroll = 0
-				elseif mkinput == "pageup" then
+				else
 					fieldscroll = fieldscroll + distance
 					if fieldscroll > 0 then
 						fieldscroll = 0
@@ -2140,7 +2140,7 @@ function handle_scrolling(viakeyboard, mkinput, customdistance, x, y)
 				local upperbound = (#topdialog.fields[k][7])*8-8*topdialog.fields[k][12]
 				if mkinput == "end" and not cfistext then
 					fieldscroll = math.min(-upperbound, 0)
-				elseif mkinput == "pagedown" then
+				else
 					fieldscroll = fieldscroll - distance
 					if -fieldscroll > upperbound then
 						fieldscroll = math.min(-upperbound, 0)