7a1bf495efdbfc5838a062385884bf99d5ef4ac5
Dav999 <dav999@tolp2.nl>
2023-05-30 21:51:18 +0200

    Check jit ~= nil before calling jit.off()
    This is probably not necessary - I coincidentally noticed LÖVE 0.8.0
    doesn't have the jit module and this line makes Ved('s error screen)
    completely and silently fail to open with no message whatsoever. And
    checking for jit being nil fixes it. So just in case the jit module is
    missing in some real-life scenarios, let's just skip turning jit off in
    that case.
    
    [pre15]

--- Source/conf.lua
+++ Source/conf.lua
@@ -1,4 +1,6 @@
-jit.off() -- temporary for 1.10.2
+if jit ~= nil then
+	jit.off() -- temporary for 1.10.2
+end
 
 function love.conf(t)
 	if love._version_major == nil or (love._version_major == 0 and love._version_minor <= 8) then
