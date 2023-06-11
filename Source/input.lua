Daaaav/Ved

commit 2da88b62818d1f8d22788f08ef513c35441809ab

Make up/down in start/end of text input jump to start/end of line
If your cursor is in the middle of the last line and you press Down,
it will now act as if you pressed End, and if your cursor is in the
middle of the first line and you press Up, it will now act as if you
pressed Home.

[pre03]

Source/commitversion
@@ -1,1 +1,1 @@
-2
+3

Source/input.lua
@@ -612,8 +612,10 @@ function input.movey(id, chars)
 	local multiline = type(inputs[id]) == "table"
 
 	if not multiline then
-		if input.selpos[id] ~= nil then
-			input.clearselpos(id)
+		if chars < 0 then
+			input.leftmost(id)
+		elseif chars > 0 then
+			input.rightmost(id)
 		end
 		return
 	end
@@ -622,7 +624,14 @@ function input.movey(id, chars)
 	local x, y = input.getpos(id, true)
 
 	y = y + chars
-	y = math.min(math.max(y, 1), #lines)
+
+	if y < 1 then
+		input.leftmost(id)
+		return
+	elseif y > #lines then
+		input.rightmost(id)
+		return
+	end
 
 	input.pos[id][2] = y
 

commit 133ea2868d6dec6374b522debb5b866876d86f48

Fix PageUp and PageDown keys in script editor
This was broken in 2da88b62818d1f8d22788f08ef513c35441809ab (1.10.2),
because it assumed if the Y position went out of bounds, it meant you
had pressed Up on the first line or Down on the last line, and set the
cursor position to leftmost/rightmost accordingly, without saving a new
Y position. Now these shortcuts work as expected again.

[pre02]

Source/commitversion
@@ -1,1 +1,1 @@
-1
+2

Source/input.lua
@@ -621,15 +621,16 @@ function input.movey(id, chars)
 	local lines = inputs[id]
 	local x, y = input.getpos(id, true)
 
-	y = y + chars
-
-	if y < 1 then
+	if y <= 1 and chars < 0 then
 		input.leftmost(id)
 		return
-	elseif y > #lines then
+	elseif y >= #lines and chars > 0 then
 		input.rightmost(id)
 		return
 	end
+
+	y = y + chars
+	y = math.min(math.max(y, 1), #lines)
 
 	input.pos[id][2] = y
 