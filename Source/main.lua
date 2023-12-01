fb4540478734d5d4a508b4bc501409954afa8b9f
Dav999 <dav999.tolp@gmail.com>
2023-12-01 02:17:56 +0100

    Change holding version number from 1.10.4 to 1.11.0
    I was gonna go straight to 2.0 as the next major version, but
    autotiling and ZIP creation make this a "significant features were
    added" version I think!
    
    [pre11]

--- Source/main.lua
+++ Source/main.lua
@@ -11,7 +11,7 @@ opt_loadlevel = nil
 opt_newlevel = false
 opt_forcelanguagescreen = false
 
-vergroups = {10,4}
+vergroups = {11,0}
 
 ver = "1." .. vergroups[1] .. "." .. vergroups[2] -- Displayed in title and used in plugin minimum version check (egrep [^aotepk]ver[^socmdygwt] *.lua -i)
 checkver = ver -- update check, displayed in crash (used to have a or b as opposed to ver)
