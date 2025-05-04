77a9e12911f743928c8f24b07adccd042c405850
Dav999 <dav999.tolp@gmail.com>
2025-05-04 03:50:04 +0200

    Stop flag drag-and-drop when reentering flags list
    If you click and drag a flag in the flags list, and then decide you
    don't want to move any flags, so you press Esc, and then re-enter the
    flags list, the flag drop would still be in progress and actually drop
    the flag wherever your mouse was. This is now fixed.
    
    [pre18]

--- Source/uis/scriptflags/load.lua
+++ Source/uis/scriptflags/load.lua
@@ -4,5 +4,12 @@ return function()
 	flags_digits = tostring(limit.flags-1):len()
 	flags_page = 0
 
+	mousepressed_flag = false
+	mousepressed_flag_x = -1
+	mousepressed_flag_y = -1
+	mousepressed_flag_num = -1
+	mousepressed_flag_name = ""
+	mousereleased_flag = false
+
 	loadflagslist()
 end
