ea83c56ab66449da01e532cf8b631a89af69ce5e
Dav999 <dav999.tolp@gmail.com>
2026-02-05 21:25:22 +0100

    Add images for new theme
    The default theme now uses shiny redesigned images (thanks Reese!)
    
    Only the button shine isn't implemented yet, that's for next commit.
    
    There's now also a classic theme which can be used to restore the old
    style, if desired.
    
    [pre04]

---
+++ Source/themes/classic/theme.lua
@@ -0,0 +1,5 @@
+info = {
+	name = "Classic",
+	author = "Dav999",
+	description = "Ved's classic theme, that was used in versions 1.x.x."
+}
--- Source/themes/default/theme.lua
+++ Source/themes/default/theme.lua
@@ -1,5 +1,5 @@
 info = {
 	name = "Default",
-	author = "Dav999",
-	description = "Ved's default theme."
+	author = "Reese Rivers and Dav999",
+	description = "Ved's default theme from Ved 2.0 onwards."
 }
