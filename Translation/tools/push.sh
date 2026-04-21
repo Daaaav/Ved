0842dfeef586be194dabdbbc98456488fa65cdf0
Dav999 <dav999.tolp@gmail.com>
2026-04-21 04:48:36 +0200

    Add NyakoFox to code contributors credits
    This is probably kinda overdue actually, so time to add her to the
    credits!
    
    [pre24]

--- Translation/tools/push.sh
+++ Translation/tools/push.sh
@@ -2,7 +2,8 @@
 
 cd ../../../Ved-translation/
 
-git commit -am 'Update English templates with new strings' || exit
+git diff
+git commit -a || exit
 git show
 read -p 'Press ENTER to push this commit, Ctrl+C to abort'
 git push
